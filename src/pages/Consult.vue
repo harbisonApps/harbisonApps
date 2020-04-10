1d9c516324387669291104c8e0374b1337d2ec32
Harbison Apps <harbisonapps@gmail.com>
2020-04-10 14:48:30 -0700

    Consult Page update

--- src/pages/Consult.vue
+++ src/pages/Consult.vue
@@ -1,8 +1,5 @@
 <template>
     <q-page padding >
-        <div class="row justify-center q-mb-md">
-            <h1>Free Consultation</h1>
-        </div>
         <div class="row justify-center q-mt-md">
             <FreeButton   @click="showFreeConsult = true"/>
             <q-dialog v-model="showFreeConsult">
@@ -10,11 +7,7 @@
             </q-dialog>
         </div>
         <div>
-            <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Eaque quod eligendi dolore aspernatur aperiam corporis recusandae. 
-            Quam repellendus, ab natus dolorem culpa quidem quas eius, nihil molestiae 
-            deserunt eveniet error!
+            <p>I am always up for a new challenge. So hit me up and lets set up your free consultation today.
             </p>
         </div>
     </q-page>
